36ef72e98ebeb80e5806a5bf652a50835e85c28d
Christina Sørensen <christina@cafkafk.com>
2023-09-16 16:52:30 +0200

    refactor(math): powerset_combined+tests
    Signed-off-by: Christina Sørensen <christina@cafkafk.com>

--- src/main.rs
+++ src/main.rs
@@ -127,7 +127,7 @@ fn main() -> std::io::Result<()> {
 
     let config = Config::load(CONFIG);
 
-    println!("{config:#?}");
+    // println!("{config:#?}");
 
     let short = r"(-[^-])";
     let long = r"(--\w+)";
@@ -173,10 +173,13 @@ fn main() -> std::io::Result<()> {
         }
     }
 
-    println!("{set:#?}");
-    println!("{set_combined:#?}");
+    // println!("{set:#?}");
+    // println!("{set_combined:#?}");
 
-    let powerset = generate_powerset(&set, config.depth.unwrap());
+    let powerset = math::generate_powerset(&set, config.depth.unwrap());
+    let powerset_combined = math::generate_powerset_combined(&set_combined, config.depth.unwrap());
+
+    println!("{powerset_combined:#?}");
 
     let output_strings: Vec<String> = powerset
         .iter()
@@ -190,7 +193,7 @@ fn main() -> std::io::Result<()> {
         })
         .collect();
 
-    println!("Output Strings: {:#?}", output_strings);
+    // println!("Output Strings: {:#?}", output_strings);
 
     // Create the dump directory if it doesn't exist
     let dump_path = Path::new("dump");
@@ -219,19 +222,110 @@ fn main() -> std::io::Result<()> {
     Ok(())
 }
 
-fn generate_powerset<T: Clone>(set: &[T], depth: usize) -> Vec<Vec<T>> {
-    let mut powerset = vec![vec![]];
+pub mod math {
+    pub fn generate_powerset<T: Clone>(set: &[T], depth: usize) -> Vec<Vec<T>> {
+        let mut powerset = vec![vec![]];
 
-    for item in set.iter() {
-        let current_len = powerset.len();
-        for i in 0..current_len {
-            let mut new_subset = powerset[i].clone();
-            new_subset.push(item.clone());
-            if new_subset.len() <= depth {
-                powerset.push(new_subset);
+        for item in set.iter() {
+            let current_len = powerset.len();
+            for i in 0..current_len {
+                let mut new_subset = powerset[i].clone();
+                new_subset.push(item.clone());
+                if new_subset.len() <= depth {
+                    powerset.push(new_subset);
+                }
             }
         }
-    }
 
-    powerset
+        powerset
+    }
+    pub fn generate_powerset_combined(
+        set: &[(Option<String>, Option<String>)],
+        depth: usize,
+    ) -> Vec<Vec<(Option<String>, Option<String>)>> {
+        let mut powerset = vec![vec![]];
+
+        for item in set.iter() {
+            let current_len = powerset.len();
+            for i in 0..current_len {
+                let mut new_subset = powerset[i].clone();
+                new_subset.push(item.clone());
+                if new_subset.len() <= depth {
+                    powerset.push(new_subset);
+                }
+            }
+        }
+
+        powerset
+    }
+    #[cfg(test)]
+    mod tests {
+        use super::*;
+
+        #[test]
+        fn test_generate_powerset() {
+            let set: Vec<i32> = vec![1, 2, 3];
+            let depth = 2;
+            let result = generate_powerset(&set, depth);
+            let expected = vec![
+                vec![],
+                vec![1],
+                vec![2],
+                vec![1, 2],
+                vec![3],
+                vec![1, 3],
+                vec![2, 3],
+            ];
+            assert_eq!(result, expected);
+        }
+
+        #[test]
+        fn test_generate_powerset_empty() {
+            let set: Vec<i32> = vec![];
+            let depth = 2;
+            let result = generate_powerset(&set, depth);
+            let expected: Vec<Vec<i32>> = vec![vec![]];
+            assert_eq!(result, expected);
+        }
+
+        fn test_generate_powerset_combined() {
+            let set = vec![
+                (Some("a".to_string()), Some("b".to_string())),
+                (Some("c".to_string()), None),
+                (None, Some("d".to_string())),
+                (None, None),
+            ];
+            let depth = 2;
+            let result = generate_powerset_combined(&set, depth);
+            let expected = vec![
+                vec![],
+                vec![(Some("a".to_string()), Some("b".to_string()))],
+                vec![(Some("c".to_string()), None)],
+                vec![(None, Some("d".to_string()))],
+                vec![(None, None)],
+                vec![
+                    (Some("a".to_string()), Some("b".to_string())),
+                    (Some("c".to_string()), None),
+                ],
+                vec![
+                    (Some("a".to_string()), Some("b".to_string())),
+                    (None, Some("d".to_string())),
+                ],
+                vec![(Some("a".to_string()), Some("b".to_string())), (None, None)],
+                vec![(Some("c".to_string()), None), (None, Some("d".to_string()))],
+                vec![(Some("c".to_string()), None), (None, None)],
+                vec![(None, Some("d".to_string())), (None, None)],
+            ];
+            assert_eq!(result, expected);
+        }
+
+        #[test]
+        fn test_generate_powerset_combined_empty() {
+            let set: Vec<(Option<String>, Option<String>)> = vec![];
+            let depth = 2;
+            let result = generate_powerset_combined(&set, depth);
+            let expected: Vec<Vec<(Option<String>, Option<String>)>> = vec![vec![]];
+            assert_eq!(result, expected);
+        }
+    }
 }
