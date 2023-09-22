6c9b8a74243f34071698ab04cfdbb118422046fc
Christina Sørensen <christina@cafkafk.com>
2023-09-22 15:21:59 +0200

    docs(process): document get_help
    Signed-off-by: Christina Sørensen <christina@cafkafk.com>

--- src/utils/process.rs
+++ src/utils/process.rs
@@ -1,6 +1,30 @@
 use std::io::{self};
 use std::process::{Command, Output};
 
+/// Executes a given command with the `--help` argument and returns its output.
+///
+/// This function will run the provided command with the `--help` argument and capture its output.
+/// If the command fails to execute successfully, an error will be printed to the standard error
+/// and an `io::Error` will be returned.
+///
+/// # Parameters
+///
+/// - `command`: The name of the command to be executed.
+/// - `_args`: Additional arguments for the command. (Currently unused in the function)
+///
+/// # Returns
+///
+/// Returns a `Result` containing the standard output of the command as a `Vec<u8>` on success,
+/// or an `io::Error` on failure.
+///
+/// # Examples
+///
+/// ```rust
+/// use utils::get_help;
+///
+/// let output = get_help("ls", &[]).unwrap();
+/// println!("{}", String::from_utf8_lossy(&output));
+/// ```
 pub fn get_help(command: &str, _args: &[&str]) -> Result<Vec<u8>, std::io::Error> {
     let output: Output = Command::new(command).args(["--help"]).output()?;
 
