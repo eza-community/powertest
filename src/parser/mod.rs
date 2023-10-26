f894f757a2632a21803462791d2756fd85e01ef8
Christina Sørensen <christina@cafkafk.com>
2023-10-26 15:12:06 +0200

    fix: move populate_set to own file
    Signed-off-by: Christina Sørensen <christina@cafkafk.com>

--- src/parser/mod.rs
+++ src/parser/mod.rs
@@ -1,3 +1,5 @@
 #![allow(clippy::module_inception)]
 mod parser;
+mod populate_set;
 pub use self::parser::parse;
+pub use self::populate_set::populate_set;
---
+++ src/parser/populate_set.rs
@@ -0,0 +1,63 @@
+#[inline]
+pub fn populate_set(
+    config: &crate::data::Config,
+    parse: &Vec<(Option<String>, Option<String>)>,
+    set: &mut Vec<String>,
+) {
+    for option in parse {
+        if let Some((key, stuff)) = config.commands.as_ref().unwrap().get_key_value(&option) {
+            match key {
+                (Some(left), Some(right)) => {
+                    if let Some(prefix) = stuff.prefix.as_ref() {
+                        for value in stuff.values.as_ref().unwrap() {
+                            set.push(format!("{} {} {}", prefix, left, &value));
+                            set.push(format!("{} {} {}", prefix, right, &value));
+                        }
+                    } else {
+                        for value in stuff.values.as_ref().unwrap() {
+                            set.push(format!("{} {}", left, &value));
+                            set.push(format!("{} {}", right, &value));
+                        }
+                    }
+                }
+                (Some(left), None) => {
+                    if let Some(prefix) = stuff.prefix.as_ref() {
+                        for value in stuff.values.as_ref().unwrap() {
+                            set.push(format!("{} {} {}", prefix, left, &value));
+                        }
+                    } else {
+                        for value in stuff.values.as_ref().unwrap() {
+                            set.push(format!("{} {}", left, &value));
+                        }
+                    }
+                }
+                (None, Some(right)) => {
+                    if let Some(prefix) = stuff.prefix.as_ref() {
+                        for value in stuff.values.as_ref().unwrap() {
+                            set.push(format!("{} {} {}", prefix, right, &value));
+                        }
+                    } else {
+                        for value in stuff.values.as_ref().unwrap() {
+                            set.push(format!("{} {}", right, &value));
+                        }
+                    }
+                }
+                (None, None) => todo!(),
+            }
+        } else {
+            match &option {
+                (Some(left), Some(right)) => {
+                    set.push(left.to_string());
+                    set.push(right.to_string());
+                }
+                (Some(left), None) => {
+                    set.push(left.to_string());
+                }
+                (None, Some(right)) => {
+                    set.push(right.to_string());
+                }
+                (None, None) => todo!(),
+            }
+        }
+    }
+}
