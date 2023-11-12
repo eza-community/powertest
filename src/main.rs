4cfc3a1752b54445fb831b55ea8db7f587d8dec7
Christina Sørensen <christina@cafkafk.com>
2023-11-12 10:20:53 +0100

    fix: hotfix for eza
    Not pretty but who is? :s
    
    Signed-off-by: Christina Sørensen <christina@cafkafk.com>

--- src/main.rs
+++ src/main.rs
@@ -54,13 +54,15 @@ fn main() -> std::io::Result<()> {
         // If --stdin set
         if stdin > &0 {
             parse = crate::parser::parse(std::io::stdin().lock());
-        } else if let Some(ref run) = config.gen_binary {
-            parse = match crate::utils::get_help(run, &[]) {
-                Ok(parse) => crate::parser::parse(BufReader::new(parse.as_slice())),
-                Err(e) => panic!("{:?}, {run:#?}", e),
-            }
-        // If we don't generate at all
-        } else {
+        }
+        // else if let Some(ref run) = config.gen_binary {
+        //     parse = match crate::utils::get_help(run, &[]) {
+        //         Ok(parse) => crate::parser::parse(BufReader::new(parse.as_slice())),
+        //         Err(e) => panic!("{:?}, {run:#?}", e),
+        //     }
+        // // If we don't generate at all
+        // }
+        else {
             // HACK: We fake being a parsed help... obviously bad.
             let mut res = vec![];
             for (k, _) in config.commands.as_ref().unwrap().iter() {
@@ -95,8 +97,6 @@ fn main() -> std::io::Result<()> {
             )
         })
         .collect();
-
-    println!("{output_strings:#?}");
 
     let dump_dir = config.dump_dir.unwrap();
 
